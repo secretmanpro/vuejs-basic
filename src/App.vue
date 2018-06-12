1e4cd68a4c1fce818b0ef9eb5f596f9fe8c2e6a0
secretmanpro <secretmanpro@gmail.com>
2018-06-12 16:17:33 +0700

    run first demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Prject1/>
+    <!-- <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Project1 from './components/Project1.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Project1
   }
 }
 </script>
--- src/components/Project1.vue
+++ src/components/Project1.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    Project1
+    <!-- <h1>{{ msg }}</h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -25,13 +26,13 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Project1',
   props: {
     msg: String
   }
